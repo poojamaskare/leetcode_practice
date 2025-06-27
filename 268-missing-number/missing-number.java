d2d23454ad1b7ca4031a4f96ce4303e6dabd7713
Pooja Maskare <poojamaskare027@gmail.com>
2025-06-27 15:05:10 +0530

    Time: 0 ms (100.00%) | Memory: 45.7 MB (21.63%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int range = nums.length;
+
+        int actualsum=   (range*(range+1))/2;    //n*(n+1)/2
+
+        int currentsum= 0;
+
+        for(int i=0;i<nums.length;i++)
+        {
+            currentsum= currentsum + nums[i];
+        }
+
+        int ans = actualsum - currentsum;
+        return ans ;
+    }
+    
+}
